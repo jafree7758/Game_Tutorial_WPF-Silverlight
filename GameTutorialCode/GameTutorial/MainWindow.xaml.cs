249b2433609c1113736c967e35330fc198d19790
xiejl <xiejl@janezt.com>
2016-08-10 15:46:18 +0800

    lesson 4: 让2D人物跑动
    Signed-off-by: xiejl <xiejl@janezt.com>

--- GameTutorialCode/GameTutorial/MainWindow.xaml.cs
+++ GameTutorialCode/GameTutorial/MainWindow.xaml.cs
@@ -22,63 +22,49 @@ namespace GameTutorial
     /// </summary>
     public partial class MainWindow : Window
     {
-        Rectangle rect;         //创建一个方块作为演示对象
-        double speed = 20;      //设置移动速度
-        double speedX = 1;
-        double speedY = 1;
-        Point moveTo;           //设置移动目标
-        bool moveFlag = false;  // 为false时表示不用移动
+        //Rectangle rect;         //创建一个方块作为演示对象
+        //double speed = 20;      //设置移动速度
+        //double speedX = 1;
+        //double speedY = 1;
+        //Point moveTo;           //设置移动目标
+        //bool moveFlag = false;  // 为false时表示不用移动
+
+        int count = 1;
+        Image Spirit;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            rect = new Rectangle();
-            rect.Fill = new SolidColorBrush(Colors.Green);
-            rect.Width = 50;
-            rect.Height = 50;
-            rect.RadiusX = 5;
-            rect.RadiusY = 5;
-            Carrier.Children.Add(rect);
-            Canvas.SetLeft(rect, 0);
-            Canvas.SetTop(rect, 0);
+            Spirit = new Image();
+            Spirit.Width = 150;
+            Spirit.Height = 150;
+            Carrier.Children.Add(Spirit);
+
+            Canvas.SetLeft(Spirit, 320);
+            Canvas.SetTop(Spirit, 220);
 
             //定义线程
             DispatcherTimer dispatcherTimer = new DispatcherTimer(DispatcherPriority.Normal);
             dispatcherTimer.Tick += new EventHandler(Timer_Tick);
-            dispatcherTimer.Interval = TimeSpan.FromMilliseconds(10); //重复间隔
+            dispatcherTimer.Interval = TimeSpan.FromMilliseconds(200); //重复间隔
             dispatcherTimer.Start();
         }
 
-        private void Carrier_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            moveTo = e.GetPosition(Carrier);
+        //private void Carrier_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        //{
+        //    moveTo = e.GetPosition(Carrier);
 
-            speedX = speed * Math.Cos(Math.Atan2(Math.Abs(moveTo.Y - Canvas.GetTop(rect)), Math.Abs(moveTo.X - Canvas.GetLeft(rect))));
-            speedY = speed * Math.Sin(Math.Atan2(Math.Abs(moveTo.Y - Canvas.GetTop(rect)), Math.Abs(moveTo.X - Canvas.GetLeft(rect))));
-            moveFlag = true;
-        }
+        //    speedX = speed * Math.Cos(Math.Atan2(Math.Abs(moveTo.Y - Canvas.GetTop(rect)), Math.Abs(moveTo.X - Canvas.GetLeft(rect))));
+        //    speedY = speed * Math.Sin(Math.Atan2(Math.Abs(moveTo.Y - Canvas.GetTop(rect)), Math.Abs(moveTo.X - Canvas.GetLeft(rect))));
+        //    moveFlag = true;
+        //}
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            double rect_X = Canvas.GetLeft(rect);
-            double rect_Y = Canvas.GetTop(rect);
+            Spirit.Source = new BitmapImage(new Uri(@"Player\" + count + ".png", UriKind.Relative));
 
-            if (moveFlag)
-            {
-                Canvas.SetLeft(rect, rect_X + (rect_X < moveTo.X ? speedX : -speedX));
-                Canvas.SetTop(rect, rect_Y + (rect_Y < moveTo.Y ? speedY : -speedY));
-            }
-            else
-            {
-                Canvas.SetLeft(rect, moveTo.X);
-                Canvas.SetTop(rect, moveTo.Y);
-            }
-            
-            if ((Math.Abs(rect_X - moveTo.X) <= speed) && (Math.Abs(rect_Y - moveTo.Y) <= speed))
-            {
-                moveFlag = false;
-            }
+            count = count == 7 ? 0 : count + 1;
         }
     }
 }
