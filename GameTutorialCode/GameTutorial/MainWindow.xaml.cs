96115ad4b26c4b3d1f2d851f2de21f55ebce6baa
xiejl <xiejl@janezt.com>
2016-08-10 17:19:54 +0800

    lesson 6: 完美移动
    让人物移动到鼠标所在位置
    改进：
    1. 移动到指定位置后，人物不再运动，截取大图的第一张小图作为静止时的图
    2. 移动的目的位置改为人物的脚底
    
    Signed-off-by: xiejl <xiejl@janezt.com>

--- GameTutorialCode/GameTutorial/MainWindow.xaml.cs
+++ GameTutorialCode/GameTutorial/MainWindow.xaml.cs
@@ -24,18 +24,22 @@ namespace GameTutorial
     {
         int count = 1;
         Image Spirit;
+        Storyboard storyboard;
+        Point moveTo;
 
         public MainWindow()
         {
             InitializeComponent();
 
+            moveTo.X = 0;
+            moveTo.Y = 0;
             Spirit = new Image();
             Spirit.Width = 150;
             Spirit.Height = 150;
             Carrier.Children.Add(Spirit);
 
-            Canvas.SetLeft(Spirit, 320);
-            Canvas.SetTop(Spirit, 220);
+            Canvas.SetLeft(Spirit, 0);
+            Canvas.SetTop(Spirit, 0);
 
             //定义线程
             DispatcherTimer dispatcherTimer = new DispatcherTimer(DispatcherPriority.Normal);
@@ -46,10 +50,64 @@ namespace GameTutorial
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            //Spirit.Source = new BitmapImage(new Uri(@"Player\" + count + ".png", UriKind.Relative));
-            Spirit.Source = cutImage(@"Player\PlayerMagic.png", count * 150, 0, 150, 150);
+            if ((moveTo.X == Canvas.GetLeft(Spirit)) && (moveTo.Y == Canvas.GetTop(Spirit)))
+            {
+                Spirit.Source = cutImage(@"Player\PlayerMagic.png", 0, 0, 150, 150);
+            }
+            else
+            {
+                Spirit.Source = new BitmapImage((new Uri(@"Player\" + count + ".png", UriKind.Relative)));
+                count = count == 7 ? 0 : count + 1;
+            }
+        }
 
-            count = count == 9 ? 0 : count + 1;
+        private void Carrier_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            moveTo = e.GetPosition(Carrier);
+
+            moveTo.X = moveTo.X - 75;   // 转换成脚底位置
+            moveTo.Y = moveTo.Y - 115;  // 转换成脚底位置
+
+            Move(moveTo);
+        }
+
+
+        private void Move(Point p)
+        {
+
+            //创建移动动画
+            storyboard = new Storyboard();
+
+            //创建X轴方向动画
+            DoubleAnimation doubleAnimation = new DoubleAnimation(
+              Canvas.GetLeft(Spirit),
+              p.X,
+              new Duration(TimeSpan.FromSeconds(1))
+            );
+
+            Storyboard.SetTarget(doubleAnimation, Spirit);
+            Storyboard.SetTargetProperty(doubleAnimation, new PropertyPath("(Canvas.Left)"));
+            storyboard.Children.Add(doubleAnimation);
+
+            //创建Y轴方向动画
+            doubleAnimation = new DoubleAnimation(
+              Canvas.GetTop(Spirit),
+              p.Y,
+              new Duration(TimeSpan.FromSeconds(1))
+            );
+
+            Storyboard.SetTarget(doubleAnimation, Spirit);
+            Storyboard.SetTargetProperty(doubleAnimation, new PropertyPath("(Canvas.Top)"));
+            storyboard.Children.Add(doubleAnimation);
+
+            //将动画动态加载进资源内
+            if (!Resources.Contains("rectAnimation"))
+            {
+                Resources.Add("rectAnimation", storyboard);
+            }
+
+            //动画播放
+            storyboard.Begin();
         }
 
         /// <summary>
